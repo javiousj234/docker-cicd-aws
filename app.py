37c5caec431bec5182dad25a426997612ca4491d
Javious Johnson <javiousj234@gmail.com>
2026-09-16 14:52:31 -0500

    Initial Docker app with automated tests

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify(
+        message="Docker CI/CD project - version 2",
+        status="running"
+    )
+
+
+@app.route("/health")
+def health():
+    return jsonify(status="healthy")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
